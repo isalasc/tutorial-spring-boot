c6d5f829baea3b0f177b5175094fcd8310015348
Iván Salas Corrales <isalasc716@gmail.com>
2018-12-12 21:03:08 +0100

    Actualizaciones MVC

--- src/main/java/com/programandoointentandolo/tsb/controller/ProductoController.java
+++ src/main/java/com/programandoointentandolo/tsb/controller/ProductoController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.programandoointentandolo.tsb.entity.Producto;
 import com.programandoointentandolo.tsb.service.ProductoService;
@@ -17,6 +19,9 @@ import com.programandoointentandolo.tsb.service.ProductoService;
 @Controller
 @RequestMapping("productos")
 public class ProductoController {
+	
+	public static final String VISTA_LISTA = "lista";
+	public static final String VISTA_FORMULARIO = "formulario";
 	
 	@Value("${aplicacion.nombre}")
 	private String nombreAplicacion;
@@ -29,7 +34,25 @@ public class ProductoController {
 		model.addAttribute("titulo", nombreAplicacion);
 		model.addAttribute("productos", productoService.obtenerTodosProductos());
 
-		return "lista";
+		return VISTA_LISTA;
+	}
+	
+	@GetMapping(value = "/listaModelMap")
+	public String listarModelMap(ModelMap model) {
+		model.addAttribute("titulo", nombreAplicacion);
+		model.addAttribute("productos", productoService.obtenerTodosProductos());
+
+		return VISTA_LISTA;
+	}
+	
+	@GetMapping("/listaModelAndView")
+	public ModelAndView listarModelAndView() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("titulo", nombreAplicacion);
+		mav.addObject("productos", productoService.obtenerTodosProductos());
+		mav.setViewName(VISTA_LISTA);
+
+		return mav;
 	}
 	
 	@GetMapping("/crear")
@@ -38,22 +61,21 @@ public class ProductoController {
 		model.put("producto", producto);
 		model.put("titulo", nombreAplicacion);
 		
-		return "formulario";
+		return VISTA_FORMULARIO;
 	}
 	
-	@PostMapping(value="/guardar")
-	public String guardar(Producto producto, Model model) {
+	@PostMapping("/guardar")
+	public String guardar(Producto producto) {
 		productoService.guardar(producto);
 		
-		return "redirect:lista";
+		return "redirect:" + VISTA_LISTA;
 	}
 	
-	
-	@GetMapping(value="/eliminar/{id}")
+	@GetMapping("/eliminar/{id}")
 	public String eliminar(@PathVariable(value="id") Integer idProducto) {
 		productoService.eliminar(idProducto);
 
-		return "redirect:../lista";
+		return "redirect:../" + VISTA_LISTA;
 	}
 
 }
