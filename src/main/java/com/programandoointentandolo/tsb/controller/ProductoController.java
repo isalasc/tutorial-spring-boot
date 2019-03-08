527d0a87fbcfa5d5ccdbeae5a5296d73255a9e38
Iván Salas Corrales <isalasc716@gmail.com>
2019-03-08 21:51:02 +0100

    Validación del formulario de productos

--- src/main/java/com/programandoointentandolo/tsb/controller/ProductoController.java
+++ src/main/java/com/programandoointentandolo/tsb/controller/ProductoController.java
@@ -2,11 +2,14 @@ package com.programandoointentandolo.tsb.controller;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,7 +68,11 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/guardar")
-	public String guardar(Producto producto) {
+	public String guardar(@Valid Producto producto, BindingResult result) {
+		if (result.hasErrors()) {			
+			return VISTA_FORMULARIO;
+		}
+		
 		productoService.guardar(producto);
 		
 		return "redirect:" + VISTA_LISTA;
--- src/main/java/com/programandoointentandolo/tsb/entity/Producto.java
+++ src/main/java/com/programandoointentandolo/tsb/entity/Producto.java
@@ -4,6 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Producto {
@@ -11,11 +17,17 @@ public class Producto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
+	@Size(min = 3, max = 8)
+	@Pattern(regexp = "[A-Z0-9]+")
 	private String codigo;
 
+	@NotEmpty
 	private String nombre;
-	
+
+	@NotNull
+	@Min(2)
+	@Max(100)
 	private Double precio;
 
 	public Producto() {
